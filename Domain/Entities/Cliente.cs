3b8f294df8472ebdd06a840dd202c10eafec8918
Edwin Camilo Corzo Sanchez <194437744+Edwincamilocorzosanchez@users.noreply.github.com>
2025-10-10 13:20:32 -0500

    fix: :bug: Correcion de errores

--- Domain/Entities/Cliente.cs
+++ Domain/Entities/Cliente.cs
@@ -10,21 +10,18 @@ public class Cliente : BaseEntity
     public CorreoVO Correo { get; set; } = null!;
     public TelefonoVO Telefono { get; set; } = null!;
     public DireccionVO Direccion { get; set; } = null!;
-    // valor por defecto true
     public EstadoVO IsActive { get; set; } = new EstadoVO(true);
 
-    // Clave foránea real (int)
     public int UserId { get; set; }
-    // Relación 1:1 con usuario
     public UserMember User { get; set; } = null!;
-    // relacion uno a muchos
+
     public ICollection<Vehiculo> Vehiculos { get; set; } = new List<Vehiculo>();
 
-    // constructores
     public Cliente()
     {
-        IsActive = new EstadoVO(true); // garantiza que nunca será null
+        IsActive = new EstadoVO(true);
     }
+
     public Cliente(IdVO id, NombreVO nombre, CorreoVO correo, TelefonoVO telefono, DireccionVO direccion, EstadoVO? isActive, int userId)
     {
         Id = id;
@@ -32,7 +29,8 @@ public class Cliente : BaseEntity
         Correo = correo;
         Telefono = telefono;
         Direccion = direccion;
-        IsActive = isActive ?? new EstadoVO(true); // valor por defecto si no se pasa
+        IsActive = isActive ?? new EstadoVO(true);
         UserId = userId;
     }
 }
+

9c7f411442051595cb9ad70d6a7cc5605a664edf
servorx <191423123+servorx@users.noreply.github.com>
2025-10-13 21:44:54 -0500

    feat: :memo: new readme

--- Api/Controllers/AdministradoresController.cs
+++ Api/Controllers/AdministradoresController.cs
@@ -8,9 +8,12 @@ using Domain.Entities;
 using Domain.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Api.Controllers;
 
+// el autorizado sirve para que el usuario al realizar la solicitud deba de validar el token
+[Authorize(Roles = "Administrador")]
 public class AdministradoresController : BaseApiController
 {
     private readonly IAdministradorService _service;
